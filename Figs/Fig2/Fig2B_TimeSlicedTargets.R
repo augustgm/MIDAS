d603ab65c8fe1d908758d7fd3a3df5d2ca2dfd9a
augustgm <91262878+augustgm@users.noreply.github.com>
2026-01-23 08:35:53 +0000

    Update Fig2B_TimeSlicedTargets.R

--- Figs/Fig2/Fig2B_TimeSlicedTargets.R
+++ Figs/Fig2/Fig2B_TimeSlicedTargets.R
@@ -7,15 +7,11 @@ library(readxl)
 library(ggplot2)
 library(ggpubr)
 
-
 rm(list = ls())
 
 ## read in data - using this allows to do the whole sampling to get the answer - for just plotting from source data see below
-predictions = readxl::read_excel(path = "C:/Users/Marce/OneDrive/Documents/Rscripts-MBPhD/MIDAS/PaperFigures/2025-11-ReviewUpdate/Fig2_SourceData.xlsx",
+predictions = readxl::read_excel(path = "Fig2_SourceData.xlsx",
                                  sheet = "Fig2B") %>% as.data.frame()
-
-#predictions = read.csv(header = T, stringsAsFactors = F,
-#                       file = "C:/Users/Marce/OneDrive/Documents/Rscripts-MBPhD/MIDAS/PaperFigures/Fig2B_allTimeSliced_forPlot.csv")
 
 ## randomly sample
 null_dist = data.frame()
@@ -35,17 +31,6 @@ plot_df_pred = data.frame(proba = predictions[predictions$new_targs == "yes", "m
 plot_df_rand = data.frame(proba = null_dist$rand_mean_prob, source = "random")
 plot_df = rbind(plot_df_pred, plot_df_rand)
 
-
-# this is equivalent to the read_excel one below.
-#plot_df = read.csv(header = T, stringsAsFactors = F,
-#                   file = "C:/Users/Marce/OneDrive/Documents/Rscripts-MBPhD/MIDAS/PaperFigures/2025-11-ReviewUpdate/Fig2B_allTimeSlicedVsRandom_inclNull_forPlot.csv")
-
-##### from source data
-#plot_df = readxl::read_excel(path = "C:/Users/Marce/OneDrive/Documents/Rscripts-MBPhD/MIDAS/PaperFigures/2025-11-ReviewUpdate/Fig2_SourceData_old.xlsx",
-#                             sheet = "Fig2B") %>% as.data.frame()
-
-
-
 ggplot(data = plot_df, 
        mapping = aes(x = source, y = proba, fill = source)) +
   #geom_violin(linewidth = 1.3) +
@@ -55,3 +40,4 @@ ggplot(data = plot_df,
   scale_fill_manual(values = c("random" = "#EFB75E", "time-sliced" = "#ED7A5D")) +
   theme_bw() +
   theme(text = element_text(size = 18))
+
